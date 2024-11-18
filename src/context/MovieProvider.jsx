1b0453018ec16e4fd3efc1dc50dcb8b1135fc3e1
Seckin Ozturk <franseck102@gmail.com>
2024-11-17 22:37:29 -0500

    movieprovider

--- src/context/MovieProvider.jsx
+++ src/context/MovieProvider.jsx
@@ -1,4 +1,6 @@
-import React, { createContext } from 'react'
+import React, { createContext, useContext, useState } from 'react'
+import axios from 'axios';
+
 
 export const MovieContext = createContext();
 
@@ -6,8 +8,28 @@ export const useMovieContext = () => {
     return useContext(MovieContext);
   };
 
+  const API_KEY = process.env.REACT_APP_TMDB_KEY;
+  const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState (false)
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (URL) => {
+    setLoading(true);
+    axios
+      .get(URL)
+      .then((res) => setMovies(res.data.results))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  };
+
+
 const MovieProvider = ({children}) => {
-  return <MovieContext.Provider value={null}>{children}</MovieContext.Provider>
+  return <MovieContext.Provider value={{movies, getMovies, loading}}>{children}</MovieContext.Provider>
 };
 
 export default MovieProvider
