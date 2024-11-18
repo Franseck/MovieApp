05f17cadece4f9ab5ae351e1580f752b992f0fed
Seckin Ozturk <franseck102@gmail.com>
2024-11-17 22:03:26 -0500

    movieprovider

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import AppRouter from "./router/AppRouter";
 import AuthProvider from "./context/AuthProvider";
 import { ToastContainer } from "react-toastify";
+import MovieProvider from "./context/MovieProvider";
 
 
 const App = () => {
   return (
 <div>
       <AuthProvider>
+<MovieProvider>
         <AppRouter />
+</MovieProvider>
       </AuthProvider>
       <ToastContainer/>
       </div>
